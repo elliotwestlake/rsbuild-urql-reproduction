6ea27a990e8c0ea71284a260c368b5dc9a479d97
Elliot Westlake <17042376+elliotwestlake@users.noreply.github.com>
2024-05-28 09:48:16 +0100

    chore: fix reproduction

--- src/index.tsx
+++ src/index.tsx
@@ -3,35 +3,34 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "urql";
 
-import { cacheExchange, Client, createClient, fetchExchange } from "urql";
+import { cacheExchange, createClient, fetchExchange } from "urql";
 
 import { authExchange } from "@urql/exchange-auth";
 
-const client = (): Client =>
-  createClient({
-    url: "https://dex-server.herokuapp.com/",
-    exchanges: [
-      cacheExchange,
-      authExchange(async (utils) => {
-        const token = "hello";
+const client = createClient({
+  url: "https://dex-server.herokuapp.com/",
+  exchanges: [
+    cacheExchange,
+    authExchange(async (utils) => {
+      const token = "hello";
 
-        return {
-          addAuthToOperation: (operation) => {
-            if (!token) return operation;
+      return {
+        addAuthToOperation: (operation) => {
+          if (!token) return operation;
 
-            return utils.appendHeaders(operation, {
-              Authorization: `Bearer ${token}`,
-            });
-          },
-          // getAccessTokenSilently handles errors and redirects for us
-          didAuthError: () => false,
-          refreshAuth: async () => {},
-        };
-      }),
-      fetchExchange,
-    ],
-    requestPolicy: "cache-and-network",
-  });
+          return utils.appendHeaders(operation, {
+            Authorization: `Bearer ${token}`,
+          });
+        },
+        // getAccessTokenSilently handles errors and redirects for us
+        didAuthError: () => false,
+        refreshAuth: async () => {},
+      };
+    }),
+    fetchExchange,
+  ],
+  requestPolicy: "cache-and-network",
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
